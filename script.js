7a117842759f59e582369f7e8b14e9edb97e5be3
lucas barbosa <lucasbarbosa81612@gmail.com>
2026-08-17 17:20:08 -0300

    feat: adiciona função ao botão de regras

---
+++ script.js
@@ -0,0 +1,6 @@
+const btnRegras = document.querySelector(".btn-regras")
+btnRegras.addEventListener('click', redirecionarParaRegras) 
+
+function redirecionarParaRegras() {
+    console.log("Funcionando!")
+}
